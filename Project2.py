7f469d7bb73ff90e9303f3f7cf840abfeaebf3b9
Deep Patel <deeppatel06022003@gmail.com>
2026-02-16 14:29:32 +0530

    Update Project2.py

--- Project2.py
+++ Project2.py
@@ -4,7 +4,6 @@ bill = float(input("What was the total bill? $"))
 tip = int(input("What percentage tip would you like to split the bill? 10,12, or 15? "))
 new_bill = (bill * (tip/100))
 new_totalBill = bill + new_bill
-print(new_totalBill)
 split = float(input("How many people to split the bill? "))
-split_bill = new_totalBill / split
+split_bill = round(new_totalBill / split ,2)
 print(f"Each person should pay: ${split_bill}")
